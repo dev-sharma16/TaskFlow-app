0094c66a167debd55507a8818e3e51d9670b0823
Dev Sharma <devsharma1683@gmail.com>
2025-04-17 15:13:56 +0530

    imported all the components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,28 @@
 import Button from "./Button";
 import Input from "./Input";
+import Login from "./Login"
+import SignUp from "./SignUp"
+import Logo from "./Logo"
+import AuthLayout from "./AuthLayout"
+import Select from './Select'
+import TaskForm from "./task-form/TaskForm";
+import Container from "./container/Container";
+import Footer from "./footer/Footer"
+import Header from "./header/Header"
+import LogoutBtn from "./header/LogoutBtn"
+
 
 export{
     Button,
     Input,
+    Login,
+    SignUp,
+    Logo,
+    AuthLayout,
+    Select,
+    TaskForm,
+    Container,
+    Footer,
+    Header,
+    LogoutBtn,
 }
