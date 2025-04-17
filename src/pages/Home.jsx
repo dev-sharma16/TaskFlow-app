3e02a1593e711de85e5c5a939068fdc16880791b
Dev Sharma <devsharma1683@gmail.com>
2025-04-17 17:24:22 +0530

    home or landing page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+    return (
+      <section className="text-center py-16">
+        <h1 className="text-4xl font-bold mb-4">Welcome to taskFlow 🚀</h1>
+        <p className="text-lg text-gray-700">
+          Organize your tasks with ease. Signup and get started now!
+        </p>
+      </section>
+    );
+}
