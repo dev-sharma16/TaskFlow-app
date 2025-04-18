21ef5b3b077bdaafde3a3cb8371c2ff56973906e
Dev Sharma <devsharma1683@gmail.com>
2025-04-18 15:59:58 +0530

    rendering task on status based rather then priority basis

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -7,9 +7,9 @@ import { TaskCard } from "../components";
 function Dashboard() {
   
   const [tasks, setTasks] = useState({
-    low: [],
-    medium: [],
-    high: [],
+    todo: [],
+    inProgress: [],
+    done: [],
   })
   
   const [loading,setLoading] = useState(true)
@@ -27,20 +27,19 @@ function Dashboard() {
         const response = await service.getTaskByUser(userData.$id)
 
         const grouped = {
-          low:[],
-          medium:[],
-          high:[],
+          todo:[],
+          inProgress:[],
+          done:[],
         }
 
         response.documents.forEach((task)=>{
-          const priority = task.priority?.toLowerCase()
-          if(grouped[priority]){
-            grouped[priority].push(task)
+          const status = task.status?.toLowerCase()
+          if(grouped[status]){
+            grouped[status].push(task)
           }
         })
 
         setTasks(grouped)
-
 
       } 
       catch (error) {
@@ -56,31 +55,25 @@ function Dashboard() {
 
   if (loading) return <div className="text-center mt-10 text-xl">Loading tasks...</div>;
 
+  const statusLabels = {
+    todo: "To Do",
+    inprogress: "In Progress",
+    done: "Completed",
+  };
+
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
       <h1 className="text-3xl font-bold mb-6 text-center">Task Dashboard</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {["low", "medium", "high"].map((priority) => (
-          <div key={priority} className="bg-white p-4 rounded shadow">
+        {["todo", "inProgress", "done"].map((status) => (
+          <div key={status} className="bg-white p-4 rounded shadow">
             <h2 className="text-xl font-semibold capitalize mb-4 text-center text-gray-700">
-              {priority} Priority
+              {statusLabels[status]} 
             </h2>
 
-            {tasks[priority].length > 0 ? (
-              // tasks[priority].map((task) => (
-              //   <div
-              //     key={task.$id}
-              //     className="mb-4 p-3 border border-gray-300 rounded bg-gray-50"
-              //   >
-              //     <h3 className="text-lg font-medium">{task.title}</h3>
-              //     <p className="text-sm text-gray-600">{task.description}</p>
-              //     <p className="text-xs mt-2 text-gray-500">
-              //       Due: {task.dueDate || "No due date"}
-              //     </p>
-              //   </div>
-              // ))
-              tasks[priority].map((task)=>(
+            {tasks[status].length > 0 ? (
+              tasks[status].map((task)=>(
                  <TaskCard key={task.$id} task={task}/>
               ))
             ) : (
