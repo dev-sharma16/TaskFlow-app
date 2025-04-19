86bb34e5ee939f66830fc8cef8b8b88a4fc4eedb
Dev Sharma <devsharma1683@gmail.com>
2025-04-20 00:56:01 +0530

    change some text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
     return (
       <section className="text-center py-16">
-        <h1 className="text-4xl font-bold mb-4">Welcome to taskFlow 🚀</h1>
+        <h1 className="text-4xl font-bold mb-4">Welcome to TaskFlow 🚀</h1>
         <p className="text-lg text-gray-700">
           Organize your tasks with ease. Signup and get started now!
         </p>
